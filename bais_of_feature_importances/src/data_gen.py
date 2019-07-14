5431660b02f807012bf035e11f6677e5175a88b1
Masaki AOTA <aotamasakimail@gmail.com>
2019-07-14 21:38:05 +0900

    [WIP] start writing article

--- bais_of_feature_importances/src/data_gen.py
+++ bais_of_feature_importances/src/data_gen.py
@@ -251,3 +251,89 @@ def data_pattern7(random_state=2019):
     target=np.array(([1]*100)+([0]*100))
 
     return data, target
+
+def data_pattern8(random_state=2019):
+    '''
+    get artificial dataset
+
+
+    同じ分布(正規分布とか)で分散も同じだが、ある程度集約する場合(binを50個、20個、10個、3個みたいに変えてみる)
+    3シグマ区間を端として集計する
+    
+    最後の特徴量（一番荒い）だけラベルに関連付ける
+    
+    
+    5次元、200サンプル
+    '''
+    def _get_partations_and_representatives(n_bins,sigma):
+        '''
+        returns
+        ----------
+        partations ... 集計で区切るポイント
+        representative ... その集計の代表値(長さはlen(partations)-1となる)
+        '''
+        #3シグマ区間を集計の端とする(正確にはそれの一つ内側)
+        edge_min=-3*sigma
+        edge_max=3*sigma
+        width=edge_max-edge_min
+        partations=[i*width/n_bins + edge_min for i in range(n_bins+1)]
+        representative=[(left+right)/2 for left, right in\
+                        zip(partations[:-1],partations[1:])]
+
+        #端を都合の良いように書き換える
+        partations[0],partations[-1]=-np.inf,np.inf
+        
+        return partations, representative
+        
+    np.random.seed(seed=random_state)
+
+    norm=stats.norm
+    sigma=1
+    
+    #正規分布のまんまの場合
+    columns=[norm.rvs(loc=0, scale=sigma,size=(200,))]
+    
+    # bins=50の場合
+    partations, representative=_get_partations_and_representatives(50,sigma)
+    norm_tmp=norm.rvs(loc=0, scale=sigma,size=(200,))
+    for left,right,rep in zip(partations[:-1],partations[1:],representative):
+        norm_tmp[(left<=norm_tmp)&(norm_tmp<right)]=rep
+    
+    columns.append(norm_tmp)
+    
+    # bins=20の場合
+    partations, representative=_get_partations_and_representatives(20,sigma)
+    norm_tmp=norm.rvs(loc=0, scale=sigma,size=(200,))
+    for left,right,rep in zip(partations[:-1],partations[1:],representative):
+        norm_tmp[(left<=norm_tmp)&(norm_tmp<right)]=rep
+    
+    columns.append(norm_tmp)
+    
+    # bins=10の場合
+    partations, representative=_get_partations_and_representatives(10,sigma)
+    norm_tmp=norm.rvs(loc=0.5, scale=sigma,size=(100,1))
+    norm_tmp=np.vstack([norm_tmp, norm.rvs(loc=-0.5, scale=sigma,size=(100,1))]).reshape(-1)
+#     norm_tmp=norm.rvs(loc=0, scale=sigma,size=(200,))
+    for left,right,rep in zip(partations[:-1],partations[1:],representative):
+        norm_tmp[(left<=norm_tmp)&(norm_tmp<right)]=rep
+    
+    columns.append(norm_tmp)
+    
+#     # bins=3の場合
+#     partations, representative=_get_partations_and_representatives(3,sigma)
+#     norm_tmp=norm.rvs(loc=0.5, scale=sigma,size=(100,1))
+#     norm_tmp=np.vstack([norm_tmp, norm.rvs(loc=-0.5, scale=sigma,size=(100,1))]).reshape(-1)
+   
+#     for left,right,rep in zip(partations[:-1],partations[1:],representative):
+#         norm_tmp[(left<=norm_tmp)&(norm_tmp<right)]=rep
+    
+#     columns.append(norm_tmp)
+    
+    
+    #ndarrayに変換
+    data=np.array(columns).T
+    
+    # target
+    target=np.array(([1]*100)+([0]*100))
+
+    return data, target
